8521ff7dcac5b81067e9e601b0901a182c24d050
Adam Kariv <adam@everything.me>
2014-06-08 00:20:42 +0300

    Fix bug in changeling title fix - it used to remove some lines on the way...

--- processors/fix_changeline_budget_titles.py
+++ processors/fix_changeline_budget_titles.py
@@ -26,10 +26,12 @@ class fix_changeline_budget_titles(object):
             line = json.loads(line.strip())
             key = "%(year)s/%(budget_code)s" % line
             title = budgets.get(key)
-            if title != None and title != line['budget_title']:
-                line['budget_title'] = title
-                changed_num += 1
+            if title != None:
+                if title != line['budget_title']:
+                    line['budget_title'] = title
+                    changed_num += 1
             else:
                 logging.error("Failed to find title for change with key %s" % key)
+                raise Exception()
             outfile.write(json.dumps(line,sort_keys=True)+"\n")
         print "updated %d entries" % changed_num
