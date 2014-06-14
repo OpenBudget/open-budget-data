8d2eeab35a8777b7d61d96b8097ca202ad1bbf53
Adam Kariv <adam@everything.me>
2014-06-14 07:52:42 +0300

    fix budget titles in changegroups

--- processors/fix_changeline_budget_titles.py
+++ processors/fix_changeline_budget_titles.py
@@ -24,13 +24,19 @@ class fix_changeline_budget_titles(object):
         changed_num = 0
         for line in file(changes_jsons):
             line = json.loads(line.strip())
-            key = "%(year)s/%(budget_code)s" % line
-            title = budgets.get(key)
-            if title != None:
-                if title != line['budget_title']:
-                    line['budget_title'] = title
-                    changed_num += 1
+            year = line['year']
+            if line.has_key('changes'):
+                data = line['changes']
             else:
-                logging.error("Failed to find title for change with key %s" % key)
+                data = [line]
+            for datum in data:
+                key = "%s/%s" % (year, datum['budget_code'])
+                title = budgets.get(key)
+                if title != None:
+                    if title != line.get('budget_title',''):
+                        datum['budget_title'] = title
+                        changed_num += 1
+                else:
+                    logging.error("Failed to find title for change with key %s" % key)
             outfile.write(json.dumps(line,sort_keys=True)+"\n")
         print "updated %d entries" % changed_num
