de0e050315f031cae6734e6bc180c06022860905
Adam Kariv <adam@everything.me>
2015-02-18 01:25:24 +0200

    dump to psql

--- processors/dump_to_psql.py
+++ processors/dump_to_psql.py
@@ -10,15 +10,15 @@ import psycopg2
 
 class dump_to_psql(object):
 
-    def process(self,input,output,table,field_definition):
+    def process(self,input,output,table,field_definitions):
 
         conn = psycopg2.connect('dbname=obudget')
         c = conn.cursor()
         c.execute("""DROP TABLE %s;""")
         c.execute("""CREATE TABLE %s
-                     (%s);""" % (table,",".join("%s %s" % x for x in field_definition)))
+                     (%s);""" % (table,",".join("%s %s" % x for x in field_definitions)))
 
-        fields = dict(field_definition)
+        fields = dict(field_definitions)
         if input.endswith('.gz'):
             infile = gzip.GzipFile(input)
         else:
@@ -27,9 +27,9 @@ class dump_to_psql(object):
         for line in infile:
             line = line.strip()
             data = json.loads(line)
-            values = [data.get(field) for field,typ in field_definition]
+            values = [data.get(field) for field,typ in field_definitions]
             to_insert.append(values)
-        c.executemany("""INSERT INTO %s VALUES(%s)""" % (table,",".join(["?"]*len(field_definition))), to_insert)
+        c.executemany("""INSERT INTO %s VALUES(%s)""" % (table,",".join(["?"]*len(field_definitions))), to_insert)
         os.utime(output, None)
 
         logging.debug("TABLE %s got %s records" % (table,len(to_insert)))
