97753acc5f5458adf9c6eade6857ba230fa4e3bb
Adam Kariv <adam@everything.me>
2015-06-22 20:50:11 +0300

    entity matching + flags

--- processors/ids_from_exemptions.py
+++ processors/ids_from_exemptions.py
@@ -5,6 +5,24 @@ import logging
 
 class ids_from_exemptions(object):
 
+    allowed_prefixes = [
+        '0', '1', '2', '3', '4', '6', '8', '9',
+        '57', '590', '589',
+        '53', '54', '55',
+        '598', '599',
+        '5792', '5793',
+        '5892', '5893',
+        '5624', '5625', '5626', '5627',
+        '5636', '5631', '5632', '5634',
+        '5621', '5623',
+    ]
+
+    def legal_id(id, name):
+        return (id != '' and
+                name.strip(0) != '' and
+                len(id) == 9 and
+                any(id.startswith(x) for x in self.allowed_prefixes))
+
     def process(self,input,output):
 
         out = file(output,'w')
@@ -14,6 +32,6 @@ class ids_from_exemptions(object):
             if supplier_id is None: continue
             id = str(supplier_id).strip()
             name = data.get('supplier','').strip()
-            if id != '' and name != '' and len(id)==9 and id.startswith('5'):
+            if self.legal_id(id, name):
                 ret = {'id':id,'name':name,'kind':''}
                 out.write(json.dumps(ret,sort_keys=True)+'\n')
