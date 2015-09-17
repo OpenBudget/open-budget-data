202d4b49defe27dab5a2c031ade35c30166bda84
Adam Kariv <adam@everything.me>
2015-09-17 16:17:10 +0300

    better handilg of downloaded curated

--- processors/download_if_changed.py
+++ processors/download_if_changed.py
@@ -7,14 +7,15 @@ import hashlib
 import json
 
 class download_if_changed(object):
-    def process(self,input,output,url):
+    def process(self,input,output,url,filename):
         data = requests.get(url).content
         data_hash = hashlib.md5(data).hexdigest()
         current = ""
         try:
-            current = file(output).read()
+            current = file(filename).read()
         except:
             pass
         current_hash = hashlib.md5(current).hexdigest()
         if current_hash != data_hash:
-            file(output,'w').write(data)
+            file(filename,'w').write(data)
+        file(output,'w').write('OK')
