0615252379a950587ebe1f0aee643f4c5c90c7fc
Adam Kariv <adam.kariv@gmail.com>
2016-01-07 12:30:10 +0200

    better exemption scraper

--- processors/scrape_exemptions.py
+++ processors/scrape_exemptions.py
@@ -10,17 +10,10 @@ class scrape_exemptions(object):
         env = os.environ.copy()
         if PROXY is not None:
             env['PROXY'] = PROXY
-        output_dir = 'intermediates'
         cwd = 'tenders'
-        int_output = 'tenders/intermediates/processed.json'
-        try:
-            shutil.rmtree(os.path.join(cwd,output_dir))
-        except:
-            logging.debug("Didn't delete old dir, whatever")
-            pass
-        os.mkdir(os.path.join(cwd,output_dir))
-        args = ['/usr/bin/env', 'python', 'exemption_updated_records_scraper.py',
-                '--scrape=%s' % since, output_dir]
+        int_output = 'tenders/processed.json'
+        args = ['/usr/bin/env', 'python', 'exemption_scraper.py',
+                int_output, '--since=%s' % since]
         logging.info("RUNNING %s" % " ".join(args))
         logging.info("cwd='%s'" % cwd)
         scraper = subprocess.Popen(args,
